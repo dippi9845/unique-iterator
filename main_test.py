59385a78a1ab1f0c0c77b17889b7c60235fcc97c
Filippo Di Pietro <filippodp9845@gmail.com>
2022-10-08 21:18:39 +0200

    Update main_test.py
    added test_no_duplicate

--- main_test.py
+++ main_test.py
@@ -3,7 +3,7 @@ from unique_iterator import UniqueIterator
 
 class BasicTests(unittest.TestCase):
     
-    def unicity_test(self):
+    def test_unicity(self):
         uniques = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         duplicated = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
         
@@ -11,6 +11,16 @@ class BasicTests(unittest.TestCase):
             uniques.remove(i)
         
         self.assertListEqual([], uniques)
+    
+    def test_no_duplicate(self):
+        uniques = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        duplicated = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        for i in UniqueIterator(duplicated):
+            uniques.remove(i)
+        
+        self.assertListEqual([], uniques)
+        
 
 
 
