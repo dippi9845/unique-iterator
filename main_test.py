1fa9b7faa4b1e0ce576407e98128ba3a84ddbc83
Filippo Di Pietro <filippodp9845@gmail.com>
2022-10-08 21:15:34 +0200

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,18 @@
+import unittest
+from unique_iterator import UniqueIterator
+
+class BasicTests(unittest.TestCase):
+    
+    def unicity_test(self):
+        uniques = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        duplicated = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+        
+        for i in UniqueIterator(duplicated):
+            uniques.remove(i)
+        
+        self.assertListEqual([], uniques)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
