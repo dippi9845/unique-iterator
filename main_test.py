28d2724f2d17a809fca2615d14c53f4f846ec1ca
Filippo Di Pietro <filippodp9845@gmail.com>
2022-10-08 21:34:25 +0200

    Update main_test.py
    added test_long_duplicated with one milion of generations
    added a custom class for testing

--- main_test.py
+++ main_test.py
@@ -1,5 +1,6 @@
 import unittest
 from unique_iterator import UniqueIterator
+from random import randint
 
 class BasicTests(unittest.TestCase):
     
@@ -20,9 +21,81 @@ class BasicTests(unittest.TestCase):
             uniques.remove(i)
         
         self.assertListEqual([], uniques)
+    
+    def test_long_duplicated(self):
+        duplicated = [randint(1,6) for i in range(1_000_000)]
+        uniques = []
         
+        for i in duplicated:
+            if not i in uniques:
+                uniques.append(i)
+        
+        self.assertGreater(len(uniques), 0)
+        
+        for i in UniqueIterator(duplicated):
+            uniques.remove(i)
+        
+        self.assertListEqual([], uniques)
 
 
+
+class Pearson:
+    def __init__(self, name: str, surname: str, age : int) -> None:
+        self.name = name
+        self.surname = surname
+        self.age = age
+    
+    def __eq__(self, __o: object) -> bool:
+        return __o.name == self.name and __o.surname == self.surname and __o.age == self.age
+    
+    def __hash__(self) -> int:
+        return hash(hash(self.surname) + hash(self.name) + hash(self.age))
+    
+    def __str__(self) -> str:
+        return self.name + " " + self.surname + " " + str(self.age)
+
+
+
+class ObjectTests(unittest.TestCase):
+    def test_unicity(self):
+        uniques = [
+            Pearson("Antonio", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 12),
+            Pearson("Filippo", "Dippi", 11)]
+        
+        duplicated = [
+            Pearson("Antonio", "Bacillio", 11),
+            Pearson("Antonio", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 12),
+            Pearson("Federico", "Bacillio", 12),
+            Pearson("Filippo", "Dippi", 11),
+            Pearson("Filippo", "Dippi", 11)]
+        
+        for i in UniqueIterator(duplicated):
+            uniques.remove(i)
+        
+        self.assertListEqual([], uniques)
+    
+    def test_no_duplicate(self):
+        uniques = [
+            Pearson("Antonio", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 12),
+            Pearson("Filippo", "Dippi", 11)]
+        
+        duplicated = [
+            Pearson("Antonio", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 11),
+            Pearson("Federico", "Bacillio", 12),
+            Pearson("Filippo", "Dippi", 11)]
+        
+        for i in UniqueIterator(duplicated):
+            uniques.remove(i)
+        
+        self.assertListEqual([], uniques)
 
 if __name__ == '__main__':
     unittest.main()
