4691ff0ab2e3b07173f31691bff7d52e8a08af32
Filippo Di Pietro <filippodp9845@gmail.com>
2022-10-10 00:32:38 +0200

    Update performance.py
    implemented test_list

--- src/tests/performance.py
+++ src/tests/performance.py
@@ -1,5 +1,5 @@
-from gettext import npgettext
-from unique_iterator.unique_iterator import Unique
+from time import time
+from ..unique_iterator.unique_iterator import Unique
 import numpy as np
 from collections import Counter
 from random import randint
@@ -38,17 +38,51 @@ def unique2(list1):
     return rtr
 
 def unique3(list1):
+    '''
+    code taken from https://www.geeksforgeeks.org/python-get-unique-values-list/
+    '''
     x = np.array(list1)
     return " ".join(np.unique(x))
 
 def unique4(list1):
+    '''
+    code taken from https://www.geeksforgeeks.org/python-get-unique-values-list/
+    '''
     return str(*Counter(list1))
 
 def my_unique(list1):
     return " ".join(Unique(list1))
 
 def test_list(list1):
-    pass
+    s = time()
+    
+    unique1(list1)
+    
+    print("unique1 took", time() - s)
+    
+    s = time()
+    
+    unique2(list1)
+    
+    print("unique2 took", time() - s)
+    
+    s = time()
+    
+    unique3(list1)
+    
+    print("unique3 took", time() - s)
+    
+    s = time()
+    
+    unique4(list1)
+    
+    print("unique4 took", time() - s)
+    
+    s = time()
+    
+    my_unique(list1)
+    
+    print("unique5 took", time() - s)
 
 if __name__ == "__main__":
     a = [randint(1,10) for i in range(1_000)]
